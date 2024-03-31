d6a9b50fdafc7993190eeb026c2e7c8762ba021e
Mafalda Pires <mfpires111@gmail.com>
2024-03-31 16:16:59 +0100

    added modal

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,29 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import data from '../data/index.json';
+import PetModal from './PetModal';
 
 export default function MultiActionAreaCard() {
+  const [selectedPet, setSelectedPet] = useState(null);
+
+  const handleOpenModal = (pet) => {
+    setSelectedPet(pet);
+    
+  };
+
+  const handleCloseModal = () => {
+    setSelectedPet(null);
+  };
+
   return (
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '20px', justifyContent: 'center', padding: '150px', alignItems: 'stretch' }}>
       {data?.pets?.map((pet, index) => (
         <Card key={index} sx={{ maxWidth: 345, border: '1px solid transparent' }}>
-          <CardActionArea>
+          <CardActionArea onClick={() => handleOpenModal(pet)}>
             <CardMedia
               component="img"
               height="140"
@@ -40,6 +52,7 @@ export default function MultiActionAreaCard() {
           </CardActions>
         </Card>
       ))}
+      {selectedPet && <PetModal open={true} handleClose={handleCloseModal} pet={selectedPet} />}
     </div>
   );
 }
