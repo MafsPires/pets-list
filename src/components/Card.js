313785afa387a9e400555bdd519a2a9009989480
Mafalda Pires <mfpires111@gmail.com>
2024-03-31 14:59:34 +0100

    add some pets

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,23 +8,22 @@ import data from '../data/index.json';
 
 export default function MultiActionAreaCard() {
   return (
-    <>
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '20px', justifyContent: 'center', padding: '150px', alignItems: 'stretch' }}>
       {data?.pets?.map((pet, index) => (
-        <Card key={index} sx={{ maxWidth: 345 }}>
+        <Card key={index} sx={{ maxWidth: 345, border: '1px solid transparent' }}>
           <CardActionArea>
             <CardMedia
               component="img"
               height="140"
-              image= {pet.image}
+              image={pet.image}
               alt={pet.name}
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 {pet.name}
-
               </Typography>
               <Typography variant="body2" color="text.secondary">
-                {pet.type}
+                {pet.age} years old
               </Typography>
               <Typography variant="body2" color="text.secondary">
                 {pet.description}
@@ -38,6 +37,6 @@ export default function MultiActionAreaCard() {
           </CardActions>
         </Card>
       ))}
-    </>
+    </div>
   );
 }
